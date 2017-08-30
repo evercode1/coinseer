f8e502c73e2c9c6fcea6068ae58845a557bfacb2
billk <ikon321@yahoo.com>
2017-08-30 15:51:46 -0700

    models

--- app/User.php
+++ app/User.php
@@ -2,12 +2,16 @@
 
 namespace App;
 
+use App\Http\AuthTraits\OwnsRecord;
+use App\UtilityTraits\HasModelTrait;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\UserRequest;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasModelTrait, OwnsRecord;
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +19,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'is_subscribed',
+        'is_admin',
+        'status_id'
     ];
 
     /**
@@ -26,4 +35,66 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function updateUser($user, UserRequest $request)
+    {
+
+        return  $user->update(['name'  => $request->name,
+                               'is_subscribed' => $request->is_subscribed,
+                               'is_admin' => $request->is_admin,
+                               'status_id' => $request->status_id,
+        ]);
+
+
+    }
+
+    public function showAdminStatusOf($user)
+    {
+
+        return $user->is_admin ? 'Yes' : 'No';
+
+    }
+
+    public function showNewsletterStatusOf($user)
+    {
+
+        return $user->is_subscribed == 1 ? 'Yes' : 'No';
+
+    }
+
+
+    public function isAdmin()
+    {
+
+        return Auth::user()->is_admin == 1;
+    }
+
+    public function isActiveStatus()
+    {
+
+        return Auth::user()->status_id == 10;
+    }
+
+    public function socialProviders()
+    {
+
+        return $this->hasMany('App\SocialProvider');
+
+    }
+
+    public function posts()
+    {
+
+        return $this->hasMany('App\Post');
+
+
+
+    }
+
+    public function contacts()
+    {
+
+        return $this->hasMany('App\Contact');
+
+    }
 }
