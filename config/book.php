3c1c201867a27e141e57693e27b6b42deab470fd
billk <ikon321@yahoo.com>
2017-08-30 15:04:58 -0700

    config

---
+++ config/book.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Book Promotion For Signature
+    |--------------------------------------------------------------------------
+    |
+    |
+    | We set the config here so that we can keep our controllers clean.
+    | Configure each image type with an image path.
+    |
+    */
+    'promo-book' => [
+        'title'     => 'Laravel 5.4 For Beginners',
+        'url'       => 'https://leanpub.com/laravel-5-4-for-beginners'
+
+    ],
+];
