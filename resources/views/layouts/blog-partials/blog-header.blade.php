052b3c0135127020600b3ff1afeaeec1fa2f2cef
billk <ikon321@yahoo.com>
2017-09-09 17:11:12 -0700

    title change

--- resources/views/layouts/blog-partials/blog-header.blade.php
+++ resources/views/layouts/blog-partials/blog-header.blade.php
@@ -1,4 +1,4 @@
 <div class="blog-header">
     <h1 class="blog-title"><a href="/">Crypto Video Directory</a></h1>
-    <p class="lead blog-description">Videos on <i class="fa fa-btc" aria-hidden="true"></i>itcoin, ICOs, and Crypto Trading</p>
+    <p class="lead blog-description">The Best Videos on <i class="fa fa-btc" aria-hidden="true"></i>itcoin, ICOs, and Crypto Trading</p>
 </div>
