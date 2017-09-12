10015dd04f7dddd7a103ab490176f67ac0acc0d9
billk <ikon321@yahoo.com>
2017-09-11 20:49:37 -0700

    fixed blog title

--- resources/views/layouts/blog-partials/blog-header.blade.php
+++ resources/views/layouts/blog-partials/blog-header.blade.php
@@ -1,4 +1,4 @@
-<div class="blog-header">
-    <h1 class="blog-title"><a href="/">Crypto Video Directory</a></h1>
+<div>
+    <h1 class="blog-title"><a href="/">CoinSeer Video Directory</a></h1>
     <p class="lead blog-description">Currated videos on Bitcoin, ICOs, and Crypto Trading</p>
 </div>
