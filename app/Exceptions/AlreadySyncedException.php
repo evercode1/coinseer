99f2c1d7de790941ebab8452b05848863de26e72
billk <ikon321@yahoo.com>
2017-09-16 13:31:58 -0700

    exception already synced

--- app/Exceptions/AlreadySyncedException.php
+++ app/Exceptions/AlreadySyncedException.php
@@ -4,21 +4,6 @@ namespace App\Exceptions;
 class AlreadySyncedException extends \Exception
 {
 
-    public function handle($request, \Exception $exception)
-    {
-
-
-
-           if ($request->ajax()) {
-
-                return response()->json(['error' => 'Already Synced'], 500);
-
-           }
-
-           return response()->view('errors.already-synced-exception', compact('exception'), 500);
-
-
-
-    }
+   
 
 }
