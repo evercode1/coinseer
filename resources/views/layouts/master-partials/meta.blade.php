6e857bb0b5c8e17f1e55b71c1ac5b965749ebab2
billk <ikon321@yahoo.com>
2017-09-17 23:40:45 -0700

    google analytics

--- resources/views/layouts/master-partials/meta.blade.php
+++ resources/views/layouts/master-partials/meta.blade.php
@@ -4,5 +4,15 @@
 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="icon" href="../../favicon.ico">
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-106630492-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
 
 @yield('meta')
